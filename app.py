b9f0425b9c7c4d49e17e88f394490b1bb3cb4172
elrjacks <eleni.jackson217@gmail.com>
2023-06-06 13:17:26 -0500

    Part 2 finished

--- app.py
+++ app.py
@@ -2,19 +2,20 @@
 # gets a basic web server up and running and makes it
 # respond with a home.html template
 
-import connexion
 # access to flask functionality
 from flask import render_template
-
+import config
+from models import Person
 
 # flask application instance called app
-app = connexion.App(__name__, specification_dir="./")
-app.add_api("swagger.yml")
+app = config.connex_app
+app.add_api(config.basedir / "swagger.yml")
 
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    people = Person.query.all()
+    return render_template("home.html", people=people)
 
 
 if __name__ == "__main__":
---
+++ config.py
@@ -0,0 +1,23 @@
+# config.py
+
+import pathlib
+import connexion
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+# point to the directory that the program in running in
+basedir = pathlib.Path(__file__).parent.resolve()
+# create the Connexion app instance and give it the path to the directory that contains the specification file
+connex_app = connexion.App(__name__, specification_dir=basedir)
+
+# flask instance initialized by connexion
+app = connex_app.app
+# tells SQLAlchemy to use sql lite as the db and a file name people.db in the current directory as the db file
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{basedir / 'people.db'}"
+# turns the SQLAlchemy system off
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# initializes SQLAlchemy by passing the app config info to SQLAlchemy and assigning the result to the db variable
+db = SQLAlchemy(app)
+# initializes Marshmallow and allows it to work with the SQLAlchemy components attached to the app
+ma = Marshmallow(app)
